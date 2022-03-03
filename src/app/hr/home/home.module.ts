538e35c2e80b17e6e1acd3f5b467883d6d2753d3
Chad Hoang <chadhoang@gmail.com>
2022-03-02 19:06:31 -0800

    Create Hr Home Table Component

--- src/app/hr/home/home.module.ts
+++ src/app/hr/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HrHomeRoutingModule } from './home-routing.module';
 import { HrHomeComponent } from './home.component';
+import { StatusTrackingTableComponent } from './status-tracking-table/status-tracking-table.component';
 
 
 @NgModule({
   declarations: [
-    HrHomeComponent
+    HrHomeComponent,
+    StatusTrackingTableComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/hr/home/status-tracking-table/status-tracking-table.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-status-tracking-table',
+  templateUrl: './status-tracking-table.component.html',
+  styleUrls: ['./status-tracking-table.component.css']
+})
+export class StatusTrackingTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // TODO: Fetch data from backend
+  }
+
+}
