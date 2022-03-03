60f2f4c2d623b3671bfd35780a7d1f3728e09645
Pinxuan Huang <pinxhuang@gmail.com>
2022-03-03 13:16:52 -0500

    Render Employee List from backend

--- src/app/hr/employee-list/profile-table/profile-table.component.ts
+++ src/app/hr/employee-list/profile-table/profile-table.component.ts
@@ -1,39 +1,39 @@
-import {Component, OnInit} from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
-
-export interface EmployeeProfile {
-  name: string;
-  ssn: number;
-  startDate: string;
-  visaStatus: string;
-  position: number;
-}
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { EmployeeProfile } from 'src/app/models/general-models';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
-  selector: 'app-profile-table',
-  templateUrl: './profile-table.component.html',
-  styleUrls: ['./profile-table.component.css']
+	selector: 'app-profile-table',
+	templateUrl: './profile-table.component.html',
+	styleUrls: ['./profile-table.component.css']
 })
 export class ProfileTableComponent {
-  displayedColumns: string[] = ['name', 'ssn', 'start-date', 'visa-status', 'position', 'details'];
-  element_data: EmployeeProfile[] = [
-    {name: 'Davis Lee', ssn: 55123, startDate: '2020-12-12', visaStatus: 'F1(OPT/CPT)', position: 1},
-    {name: 'ADMIN admin', ssn: 886131331, startDate: '2021-07-03', visaStatus: 'L2', position: 2},
-    {name: 'Richard', ssn: 111111, startDate: '2020-03-20', visaStatus: 'H4', position: 3},
-    {name: 'Mason Lu', ssn: 11111111, startDate: '2021-06-20', visaStatus: 'Other', position: 4},
-    {name: 'Hazel Xu', ssn: 123123123, startDate: '2021-01-23', visaStatus: 'F1(OPT)', position: 5},
-    {name: 'Josh Lee', ssn: 2191923, startDate: '2020-12-21', visaStatus: 'F1(OPT)', position: 6}
-  ]
-  dataSource = new MatTableDataSource(this.element_data);
+	displayedColumns: string[] = ['name', 'ssn', 'start-date', 'visa-status', 'position', 'details'];
+	element_data: EmployeeProfile[] = [];
+	dataSource: MatTableDataSource<EmployeeProfile>;
 
-  constructor() {}
+	constructor(private employeeService: EmployeeService) { }
 
-  ngOnInit(): void {
-    // TODO: Fetch data from backend
-  }
+	ngOnInit(): void {
+		// TODO: Fetch data from backend
+		this.employeeService.getEmployeeList().subscribe((data: EmployeeProfile[]) => {
+			for (let item of data) {
+				let row: EmployeeProfile = new EmployeeProfile(
+					item.name,
+					item.ssn,
+					item.startDate,
+					item.visaStatus,
+					item.position
+				);
+				this.element_data.push(row);
+			}
+			this.dataSource = new MatTableDataSource(this.element_data);
+		});
+	}
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
+	applyFilter(event: Event) {
+		const filterValue = (event.target as HTMLInputElement).value;
+		this.dataSource.filter = filterValue.trim().toLowerCase();
+	}
 }
---
+++ src/app/models/general-models.ts
@@ -0,0 +1,11 @@
+
+
+export class EmployeeProfile {
+    constructor(
+        public name: string,
+        public ssn: number,
+        public startDate: string,
+        public visaStatus: string,
+        public position: number
+    ) { }
+}
