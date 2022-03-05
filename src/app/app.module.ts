434334ffb6aa9a051f4c33254bd97fef7c36d990
Neo Huang <47577320+Junren1996@users.noreply.github.com>
2022-03-05 01:13:18 -0600

    form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {CustomFormsModule} from "ng2-validation";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OnboardingModule } from './auth/onboarding/onboarding.module';
@@ -45,6 +46,9 @@ const routes: Routes = []
     HRNavigationComponent
   ],
   imports: [
+    FormsModule,
+    CustomFormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
