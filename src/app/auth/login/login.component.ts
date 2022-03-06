10f6d44c75841840f9d8474ac749383c3ef1efa7
Chad Hoang <chadhoang@gmail.com>
2022-03-06 15:47:27 -0800

    Login validator message

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -26,6 +26,13 @@ export class LoginComponent implements OnInit {
 		this.login.clear();
 	}
 
+	emailMatch = () => {
+		return this.login.email.toLowerCase()
+		.match(
+		  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		);
+	}
+
 	onSubmit = () => {
 		this.loginService.attemptLogin(this.login).subscribe(response => {
 			if (response == "hr") {
